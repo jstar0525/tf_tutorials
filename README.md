70d11cacbd6c773b58243f569e45c32fd7e9a0a5
JinSeong Park <jstar0525@gmail.com>
2022-08-27 00:03:50 -0700

    Add 6. Time travel with tf (Python)

--- README.md
+++ README.md
@@ -16,6 +16,8 @@ VMware (Ubuntu 18.04 amd64, ROS Melodic Desktop Full)
 
 [5. Learning about tf and time (Python)](https://jstar0525.tistory.com/326)
 
+[6. Time travel with tf (Python)](https://jstar0525.tistory.com/327)
+
 ## Reference
 
 [tf/Tutorials](http://wiki.ros.org/tf/Tutorials)
--- learning_tf/nodes/turtle_tf_listener.py
+++ learning_tf/nodes/turtle_tf_listener.py
@@ -19,13 +19,18 @@ if __name__ == '__main__':
     turtle_vel = rospy.Publisher('turtle2/cmd_vel', geometry_msgs.msg.Twist,queue_size=1)
 
     rate = rospy.Rate(10.0)
-    listener.waitForTransform("/turtle2", "/carrot1", rospy.Time(), rospy.Duration(4.0))
+    listener.waitForTransform("/turtle2", "/turtle1", rospy.Time(), rospy.Duration(4.0))
     while not rospy.is_shutdown():
         try:
-            now = rospy.Time.now()
-            listener.waitForTransform("/turtle2", "/carrot1", now, rospy.Duration(4.0))
-            (trans,rot) = listener.lookupTransform('/turtle2', '/carrot1', now)
-        except (tf.LookupException, tf.ConnectivityException):
+            now = rospy.Time.now() 
+            past = now - rospy.Duration(5.0)
+            listener.waitForTransformFull("/turtle2", now,
+                                          "/turtle1", past, 
+                                          "/world", rospy.Duration(1.0))
+            (trans,rot) = listener.lookupTransformFull('/turtle2', now,
+                                                       '/turtle1', past,
+                                                       "/world")
+        except (tf.Exception, tf.LookupException, tf.ConnectivityException):
             continue
 
         angular = 4 * math.atan2(trans[1], trans[0])
